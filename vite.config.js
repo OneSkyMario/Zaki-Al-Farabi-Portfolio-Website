ed090939798e2771bb441d31022d66ff6edaf589
OneSkyMario <zaki.alfarabi@nu.edu.kz>
2025-05-02 22:56:04 +0500

    Reconfigured prod base and routing

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/portfolio-website/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
